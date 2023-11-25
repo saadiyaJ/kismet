36665711967782bd71a5c76b3f7354795e659d9c
saadiya <saadiyajehar@gmail.com>
2023-11-25 21:24:17 +0300

    Game UI improved

--- lib/view/random_number_generator.dart
+++ lib/view/random_number_generator.dart
@@ -10,12 +10,11 @@ class RandomNumberGenerator extends StatefulWidget {
 }
 
 class _RandomNumberGeneratorState extends State<RandomNumberGenerator> {
-
   List<GameModel> game = [];
 
   void _getInitialInfo() {
-    game = GameModel.getGame();}
-
+    game = GameModel.getGame();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +22,48 @@ class _RandomNumberGeneratorState extends State<RandomNumberGenerator> {
     return Scaffold(
       appBar: CustomAppBar(title: "Kismet"),
       body: Column(
-        children:[
-          Padding(padding: EdgeInsets.all(25),
+        children: [
+          Padding(
+            padding: EdgeInsets.all(25),
             child: Container(
-              height: 500,
-              child: Image.asset(game[2].pathToImage[0]),
-            ),
+                height: 500,
+                child: Column(children: [
+                  Image.asset(game[2].pathToImage[0]),
+                  Text(
+                    game[2].result[0],
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    height: 45,
+                    width: 130,
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [Color(0xff9DCEFF), Color(0xff92A3FD)],
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        foregroundColor: const Color(0xffC58BF2),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Generate",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ])),
           ),
         ],
       ),
--- lib/view/roll_the_dice.dart
+++ lib/view/roll_the_dice.dart
@@ -25,15 +25,51 @@ class _RollTheDiceState extends State<RollTheDice> {
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.all(15),
+            padding: EdgeInsets.all(25),
             child: Container(
-              height: 500,
-              child: Image.asset(game[0].pathToImage[0]),
-            ),
+                height: 500,
+                child: Column(children: [
+                  Image.asset(game[0].pathToImage[0]),
+                  Text(
+                    game[0].result[0],
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    height: 45,
+                    width: 130,
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [Color(0xff9DCEFF), Color(0xff92A3FD)],
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        foregroundColor: const Color(0xffC58BF2),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Roll",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ])),
           ),
-
         ],
       ),
     );
+
+    //Image.asset(game[0].pathToImage[0]),
   }
 }
